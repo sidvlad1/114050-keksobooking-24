33eb0ef6a57f378eafdf5d5ab4b7d2a571749613
Vladimir Bogov <bogov.v.m@gmail.com>
2021-11-21 14:05:25 +0100

    +++

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+const MAX_ITEMS = 10;
+const START_AVATAR_URL = 'img/avatars/user';
+const AVATAR_USER_EXTEND = '.png';
+const TYPES = [
+  'palace', 'flat', 'house', 'bungalow', 'hotel'];
+const TIMES = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS_MOCK = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
 function getRandomNumber (startNumber = 0, endNumber = 0) {
   if (endNumber < startNumber) {
     return 0;
@@ -20,25 +32,40 @@ function getRandomFloatNumber (startNumber = 0, endNumber = 0, floatingNumber = 
   return random.toFixed(floatingNumber);
 }
 
+function createFromArray (length, array) {
+  const result = [];
+  for (let i = 0; result.length < length; i++) {
+    result.push(array[i]);
+  }
+  return result;
+};
+
+function createData (maxLength) {
+  const result = [];
+  for (let i = 0; result.length < maxLength; i++){
+    const author = {
+      avatar: START_AVATAR_URL + (i + 1) + AVATAR_USER_EXTEND,
+    };
+    const location = {
+      lat: getRandomFloatNumber (35.65000, 35.70000),
+      lng: getRandomFloatNumber (139.70000, 139.80000)
+    };
+    const offer = {
+      title: 'dfdfd',
+      address: location.lat + ',' + location.lng,
+      price: getRandomNumber (0, 1000000000000),
+      type: TYPES[getRandomNumber(0,TYPES.length - 1)],
+      rooms: getRandomNumber (0, 10),
+      guests: getRandomNumber (0, 1000),
+      features: createFromArray(getRandomNumber(0,FEATURES.length - 1), FEATURES),
+      description: 'dfdfd',
+    };
+
+    result.push({author, location, offer});
+  };
+  return result;
+};
+
 getRandomFloatNumber (1, 1, 2);
 getRandomNumber (0, 5);
 
-const author = {
-  avatar: 'img/avatars/user{{01}}.png',
-};
-
-const offer = {
-  title: 'номер в средиземноморском стиле',
-  address: '{{location.lat}}, {{location.lng}}',
-  price: '500',
-  type: 'описание объекта',
-  rooms: '1',
-  guests: '2',
-  checkin: '12:00',
-  checkout: 'описание объекта',
-  features: '{{wifi}},{{dishwasher}},{{parking}},{{washer}}',
-  description: 'красивое место',
-  photos: 'описание объекта',
-};
-
-const location = {lat, lng};
